880086fb62bab613eba0c00bc752357450684475
matthias-cs <matthias.cs.mesfin@gmail.com>
2023-07-09 12:47:39 +0300

    Created Web Pages in Pages app and configured navigation links

--- Project_Car_Zone/app_pages/urls.py
+++ Project_Car_Zone/app_pages/urls.py
@@ -3,7 +3,10 @@
 from django.urls import path
 
 # Importing project modules
-from .views import view_home_page
+from .views import view_home_page, view_about_page, view_services_page, view_contact_page 
 urlpatterns = [
     path('', view_home_page, name="url_name_for_home_page"),
+    path('about', view_about_page, name="url_name_for_about_page"),
+    path('services', view_services_page, name="url_name_for_services_page"),
+    path('contact', view_contact_page, name="url_name_for_contact_page"),
 ]
--- Project_Car_Zone/app_pages/views.py
+++ Project_Car_Zone/app_pages/views.py
@@ -6,3 +6,15 @@ from django.shortcuts import render
 # view for homepage
 def view_home_page(request):
     return render(request, 'app_pages_templates/home.html')
+
+# view for about page
+def view_about_page(request):
+    return render(request, "app_pages_templates/about.html")
+
+# view for services page
+def view_services_page(request):
+    return render(request, "app_pages_templates/services.html")
+
+# view for contact page
+def view_contact_page(request):
+    return render(request, "app_pages_templates/contact.html")
